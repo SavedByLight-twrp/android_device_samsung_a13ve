9c1fbb6ebe41908879d46751d5867a69271bf98d
Jamie Falconer <mrfluffyoven@gmail.com>
2025-08-03 08:49:39 +0100

    attempt to fix adb sideload

--- device.mk
+++ device.mk
@@ -19,6 +19,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
